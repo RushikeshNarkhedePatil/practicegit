93d3c156ef22cde8e2b99e376bf63da415e4d444
Jaydeep Godase <80196805+JaydeepGodase@users.noreply.github.com>
2022-08-29 22:05:01 +0530

    message

---
+++ first.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Jay Gajesh");
+	return 0;
+}
---
+++ second.c
@@ -0,0 +1,11 @@
+# include<stdio.h>
+
+void Demo() 
+{
+	printf("Jay Ganesh\n");
+}
+int main() 
+
+{
+	Demo();
+}
